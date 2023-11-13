59658db1e7cb205484fcf200c9d0647c0c1863b8
Aaron <aaronmwhitecouk@googlemail.com>
2023-11-13 12:47:43 +0000

    Update login input validation

--- src/Login.js
+++ src/Login.js
@@ -4,9 +4,42 @@ import PropTypes from "prop-types";
 const Login = ({ ...props }) => {
     const [emailAddress, setEmailAddress] = useState("");
     const [password, setPassword] = useState("");
+    const [emailAddressError, setEmailAddressError] = useState("")
+    const [passwordError, setPasswordError] = useState("")
 
     function handleClick(e) {
-        console.log(`${e.target.innerHTML} button was clicked`);
+        setEmailAddressError("")
+        setPasswordError("")
+
+        if (emailAddress === "") {
+            setEmailAddressError("Please enter your email address");
+            
+            return;
+        }
+
+        if (emailAddress.length < 8) {
+            setEmailAddressError("Email input field must contain more than 7 characters");
+            
+            return;
+        }
+
+        if (!/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(emailAddress)) {
+            setEmailAddressError("Email input field should be in a valid email address format (e.g. abc@test.com)");
+            
+            return;
+        }
+
+        if (password === "") {
+            setPasswordError("Please enter your password")
+
+            return;
+        }
+
+        if (password.length < 8) {
+            setPasswordError("Password input field must have more than 8 characters")
+
+            return;
+        }
     }
 
     return (
@@ -17,19 +50,21 @@ const Login = ({ ...props }) => {
                     <input
                         type="email"
                         value={emailAddress}
-                        placeholder="Email Address"
+                        placeholder="Email Address*"
                         onChange={(e) => setEmailAddress(e.target.value)}
                         className="preservica__input"
                     />
+                    <label className="preservica__input--error">{emailAddressError}</label>
                 </div>
                 <div className="preservica__input-wrapper">
                     <input
                         type="password"
                         value={password}
-                        placeholder="Password"
+                        placeholder="Password*"
                         onChange={(e) => setPassword(e.target.value)}
                         className="preservica__input"
                     />
+                    <label className="preservica__input--error">{passwordError}</label>
                 </div>
                 <div className="preservica__input-wrapper">
                     <input
